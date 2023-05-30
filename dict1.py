4f56654bbdaac4c731f15f9e70d5e241cea29373
nebula387 <nebula387@gmail.com>
2023-05-30 07:27:45 +0300

    add file

--- dict1.py
+++ dict1.py
@@ -4,17 +4,19 @@ def make_user(name,age):
     dict = {name:age}
     return dict
 
-result = make_user('Vasya', 20)
+user = make_user('Vasya', 20)
 
 
-def format_user(result):
-    for k, v in result.items():
-        return f'{k}, {v}'
+#def format_user(result):
+    #for k, v in result.items():
+        #return f'{k}, {v}'
 
+def format_user(user):
+    return f'{user["name"]}, {user["age"]}'
 
-form = format_user(result)
+form = format_user(user)
 
-print(result,form)
+print(form,user)
     
 
     
---
+++ test.py
@@ -0,0 +1,9 @@
+
+# BEGIN
+def make_user(name, age):
+    return {'name': name, 'age': age}
+
+
+def format_user(user):
+    return f'{user["name"]}, {user["age"]}'
+# END
